36908024cf729df5f5f6ac4899fda7b89b051837
rrolon <rrolon@nelson-atkins.org>
2022-08-26 17:12:33 -0500

    adding final changes with color and formatting

---
+++ Img/modal.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("modalImg");
+
+    //set tap param
+    var Tap = new Hammer.Tap({
+      taps: 1
+    });
+
+    const myNodelist = document.querySelectorAll("img");
+    
+    for (let i = 0; i < myNodelist.length; i++) {
+      manager = new Hammer.Manager(myNodelist[i])
+      manager.add(Tap);
+      manager.on('tap', openModal);
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    function openModal(e){
+        modal.style.display = "block";
+        modalImg.src = e.target.src;
+    }
+  
+});
