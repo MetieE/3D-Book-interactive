220b8212317fe0e085976b2b0fd6bfa0be6c944b
rrolon <rrolon@nelson-atkins.org>
2022-10-03 10:50:06 -0500

    changing closing zoom out scale by to make sure it zooms out all the way

--- Img/modal.js
+++ Img/modal.js
@@ -1,29 +1,69 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Get the modal
-    var modal = document.getElementById("myModal");
-    var modalImg = document.getElementById("pinch-zoom-image-id");
+  // Get the modal
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("pinch-zoom-image-id");
 
-    //set tap param
-    var Tap = new Hammer.Tap({
-      taps: 1
-    });
+  //set tap param
+  var Tap = new Hammer.Tap({
+    taps: 1
+  });
 
-    //get all the img elements
-    const myNodelist = document.querySelectorAll("img");
-    
-    // loop through img elements and assign hammer tap to each
-    for (let i = 0; i < myNodelist.length; i++) {
-      manager = new Hammer.Manager(myNodelist[i])
-      manager.add(Tap);
-      manager.on('tap', openModal);
-    }
+  //get all the img elements
+  const myNodelist = document.querySelectorAll("img");
+  
+  // loop through img elements and assign hammer tap to each
+  for (let i = 0; i < myNodelist.length; i++) {
+    manager = new Hammer.Manager(myNodelist[i])
+    manager.add(Tap);
+    manager.on('tap', openModal);
+  }
 
-    // displays modal, disables image from reloading on tap, fills modal 
-    //image from target src and runs the pinch and zoom functionality from hammer.js
-    function openModal(e){
-        modal.style.display = "block";
-        modalImg.style.pointerEvents = "none";
-        modalImg.src = e.target.src;
-        viewImg();
-    }
+  // put this in the hammer.js file to clear the hammer object on close
+  // // Get the <span> element that closes the modal
+  // var span = document.getElementsByClassName("close")[0];
+  
+  // // When the user clicks on <span> (x), close the modal
+  // span.onclick = function() {
+  //   modal.style.display = "none";
+  // }
+
+  function openModal(e){
+      modal.style.display = "block";
+      modalImg.style.pointerEvents = "none";
+      modalImg.src = e.target.src;
+      viewImg();
+  }
+
+  
+
 });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Get the modal
+//     var modal = document.getElementById("myModal");
+//     var modalImg = document.getElementById("pinch-zoom-image-id");
+
+//     //set tap param
+//     var Tap = new Hammer.Tap({
+//       taps: 1
+//     });
+
+//     //get all the img elements
+//     const myNodelist = document.querySelectorAll("img");
+    
+//     // loop through img elements and assign hammer tap to each
+//     for (let i = 0; i < myNodelist.length; i++) {
+//       manager = new Hammer.Manager(myNodelist[i])
+//       manager.add(Tap);
+//       manager.on('tap', openModal);
+//     }
+
+//     // displays modal, disables image from reloading on tap, fills modal 
+//     //image from target src and runs the pinch and zoom functionality from hammer.js
+//     function openModal(e){
+//         modal.style.display = "block";
+//         modalImg.style.pointerEvents = "none";
+//         modalImg.src = e.target.src;
+//         viewImg();
+//     }
+// });
